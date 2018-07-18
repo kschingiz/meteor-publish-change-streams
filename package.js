fbb796458383e237908a82a1064968fe5819e544
Shynggys <kschingiz@gmail.com>
2018-07-18 13:45:11 +0600

    version change

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "kschingiz:publish-change-streams",
-  version: "0.0.2",
+  version: "0.0.3",
   // Brief, one-line summary of the package.
   summary:
     "This package allow you to reactively subscribe and publish mongo change streams",
